f7af57155c942f112560589b281be40dcdc7465d
TRemigi <63087524+TRemigi@users.noreply.github.com>
2020-09-03 13:42:06 -0600

    change img declarations in script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,6 @@
 require("bootstrap");
-const img1 = require("../../assets/img/food-table.jpg");
-const img2 = require("../../assets/img/grill.jpg");
+const img1 = require("../../dist/assets/img/food-table.jpg");
+const img2 = require("../../dist/assets/img/grill.jpg");
 
 $(document).ready(function() {
 
